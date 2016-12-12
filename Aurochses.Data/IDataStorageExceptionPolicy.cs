c3e09b0db46219fd9544d5becd4d001dddd1d2f7
Vladimir Rodchenko <vladimir@aurochses.com>
2016-12-13 00:05:52 +0300

    handle exception policy

---
+++ Aurochses.Data/IDataStorageExceptionPolicy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aurochses.Data
+{
+    /// <summary>
+    /// Interface of exception policy for data storage.
+    /// </summary>
+    public interface IDataStorageExceptionPolicy<in TException>
+        where TException : Exception
+    {
+        /// <summary>
+        /// Handles the exception.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <exception cref="Exceptions.DataStorageException"></exception>
+        void HandleException(TException exception);
+    }
+}
