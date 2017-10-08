5c41063569da1a7b6b4d3c1c83b2088473097f83
Vladimir Rodchenko <vladimir@aurochses.com>
2017-10-08 21:42:02 +0300

    added QueryParameters

---
+++ src/Aurochses.Data/Query/SortRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Aurochses.Data.Query
+{
+    /// <summary>
+    /// Sort rule class.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the T entity.</typeparam>
+    /// <typeparam name="TType">The type of the T type.</typeparam>
+    public class SortRule<TEntity, TType>
+        where TEntity : IEntity<TType>
+    {
+        /// <summary>
+        /// Gets or sets sort order.
+        /// </summary>
+        public SortOrder SortOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets sort expression.
+        /// </summary>
+        public Expression<Func<TEntity, object>> Expression { get; set; }
+    }
+}
