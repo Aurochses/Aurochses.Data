257b954f55d94d3d3e3a1a9211c01a548ff2960c
Vladimir Rodchenko <vladimir@aurochses.com>
2018-01-08 11:06:21 +0300

    MsSql added

---
+++ src/Aurochses.Data/Extensions/MsSql/ColumnLengths.cs
@@ -0,0 +1,18 @@
+﻿namespace Aurochses.Data.Extensions.MsSql
+{
+    /// <summary>
+    /// Default Column Lengths.
+    /// </summary>
+    public static class ColumnLengths
+    {
+        /// <summary>
+        /// The default nvarchar length
+        /// </summary>
+        public const int DefaultNVarChar = 255;
+
+        /// <summary>
+        /// The unique name length
+        /// </summary>
+        public const int UniqueName = 50;
+    }
+}
